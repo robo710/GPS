01a5d9c14de6bfd6b9bcaa6db7f15dfa983d62a2
손찬형 <133100122+sonchanhyeong@users.noreply.github.com>
2023-10-19 16:44:41 +0900

    .

--- app/src/main/java/com/sonchan/gps/MainActivity.kt
+++ app/src/main/java/com/sonchan/gps/MainActivity.kt
@@ -2,10 +2,13 @@ package com.sonchan.gps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sonchan.gps.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
